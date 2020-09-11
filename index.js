09742beef65f0346902b8edddee260fe8f88e8d1
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-11 00:31:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function BoardMember(name homeState, training) {
+  this.name = name; 
+  this.email = email; 
+  this.training = training; 
+  
+  this.veto = function(){
+    
+    
+    
+  }
+}
